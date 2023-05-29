a9053c599ae6a916e1baa418d185e48e8af058ff
Kodam-Vinay <vinay.kodam112@gmail.com>
2023-05-29 10:54:11 +0530

    few changes

--- src/ProdRoutes.js
+++ src/ProdRoutes.js
@@ -2,8 +2,7 @@ import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
-
-
+import Chats from './pages/Chats'
 
 const ProdRoutes = () => {
   return (
@@ -11,8 +10,7 @@ const ProdRoutes = () => {
     <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/login' element={<Login/>}/>
-
-      
+    <Route path='chats' element={<Chats/>}/>
     </Routes>
     </BrowserRouter>
   )
